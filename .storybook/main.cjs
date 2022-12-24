8d10e02df21309041b53c66eb88be224e7e9a96c
guilhermeB-Dev <guilherme.2203@hotmail.com>
2022-12-24 01:33:34 +0000

    Storybook version and msw
    Changed storybook version to latest stable and added msw to project

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,22 +1,24 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx", 
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(tsx)"
   ],
   "addons": [
     "@storybook/addon-links", 
     "@storybook/addon-essentials", 
     "@storybook/addon-interactions"
   ],
-  "framework": {
-    name: "@storybook/react-vite",
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true
   },
-  docs: {
-    docsPage: "automatic"
-  },
+  "staticDirs": [
+    "../public"
+  ],
   /* If orgazination do this */
   /* viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
